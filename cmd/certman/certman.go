26f5546062cf1e9db9563de81da71d9bda7d6f8f
schrenker <zawadzkis95@gmail.com>
2020-10-09 10:36:03 +0200

    ipv4 and fqdn validators

--- cmd/certman/certman.go
+++ cmd/certman/certman.go
@@ -1,15 +1,11 @@
 package main
 
-//Certificate monitoring utility
-
 import (
 	"os"
 
 	"github.com/schrenker/certman/internal/jsonparse"
 	"github.com/schrenker/certman/internal/queue"
 )
-
-const usage = `Usage...`
 
 func main() {
 	hosts, err := jsonparse.InitHostsJSON("./configs/hosts.json")
@@ -24,8 +20,6 @@ func main() {
 
 // func verifyHostname() //Check if hostname is valid and points to an actual server
 // func verifyDomain() //Check if domain give is FQDN
-// func enqueueHosts() //Put hosts into queue
-// func processQueue() //launch goroutines with upper limit
 // func verifyCert() //Check if certificate is valid and if it matches domain
 // func parseResult() //Parse resulting date
 // func evaluateResult() //evaluate if there is risk of certificate expiring soon
---
+++ pkg/validators/validators.go
@@ -0,0 +1,42 @@
+package validators
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+//CheckIfFQDN check if supplied domain name is a Fully Qualified Domain Name
+func CheckIfFQDN(domain string) bool {
+	stdDomain := strings.TrimSpace(strings.ToLower(domain))
+
+	if len(stdDomain) == 0 {
+		return false
+	}
+
+	if stdDomain[len(stdDomain)-1] == '.' {
+		stdDomain = stdDomain[:len(stdDomain)-1]
+	}
+
+	regex := "^([a-z0-9]+(-[a-z0-9]+)*\\.)+[a-z]{2,}$"
+
+	match, _ := regexp.MatchString(regex, stdDomain)
+
+	return match
+}
+
+func CheckIfIPv4(ip string) bool {
+	octets := strings.Split(ip, ".")
+	if len(octets) != 4 {
+		return false
+	}
+	for i := range octets {
+		if len(octets[i]) == 0 {
+			return false
+		}
+		if octet, _ := strconv.Atoi(octets[i]); 0 > octet || octet > 255 {
+			return false
+		}
+	}
+	return true
+}
