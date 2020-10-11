82aee706f3d5f8e262961ad6c1451863ccadba9d
schrenker <zawadzkis95@gmail.com>
2020-10-11 17:16:55 +0200

    queue now handles vhost structs

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,7 +1,6 @@
 package queue
 
 import (
-	"strings"
 	"sync"
 
 	"github.com/schrenker/certman/internal/certutils"
@@ -9,36 +8,25 @@ import (
 )
 
 //EnqueueHosts launches goroutines that take care of checking certificates on hosts
-func EnqueueHosts(hosts map[string][]string, settings *jsonparse.Settings) {
+func EnqueueHosts(hosts []*jsonparse.Vhost, settings *jsonparse.Settings) {
 	var wg sync.WaitGroup
 	wg.Add(len(hosts))
 
 	limit := make(chan struct{}, settings.ConcurrencyLimit) //limit amount of running jobs
 
-	for k, v := range hosts {
+	for i := range hosts {
 		limit <- struct{}{}
-		go launchConnection(k, v, &wg, limit)
+		go launchConnection(hosts[i], &wg, limit)
 	}
 
 	wg.Wait()
 	close(limit)
 }
 
-func launchConnection(hostname string, domains []string, wg *sync.WaitGroup, limit chan struct{}) {
+func launchConnection(vhost *jsonparse.Vhost, wg *sync.WaitGroup, limit chan struct{}) {
 	defer wg.Done()
 
-	for _, v := range domains {
-		domain, port := splitDomainAndPort(v)
-		certutils.VerifyCertificates(hostname, domain, port)
-	}
+	certutils.VerifyCertificates(vhost)
 
 	<-limit
-}
-
-func splitDomainAndPort(domain string) (string, string) {
-	split := strings.Split(domain, ":")
-	if len(split) < 2 {
-		return split[0], "443"
-	}
-	return split[0], split[1]
 }
