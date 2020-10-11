f8bca8b4a54b40f281111c5173262fc22e15c154
schrenker <zawadzkis95@gmail.com>
2020-10-11 17:16:29 +0200

    changed parse result from map of string arrays to slice of vhost structs

--- internal/jsonparse/jsonparse.go
+++ internal/jsonparse/jsonparse.go
@@ -1,15 +1,25 @@
 package jsonparse
 
 import (
+	"crypto/x509"
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 //Settings is a struct representing all settings options available.
 type Settings struct {
 	Mail             string `json:"mail"`             //default nil
 	ConcurrencyLimit int    `json:"concurrencyLimit"` //default 10
+}
+
+type Vhost struct {
+	Hostname    string
+	Domain      string
+	Port        string
+	Certificate *x509.Certificate
+	Error       error
 }
 
 //InitSettingsJSON is a function that parses settings.json file to Settings struct
@@ -28,7 +38,7 @@ func InitSettingsJSON(path string) (*Settings, error) {
 }
 
 //InitHostsJSON parses json hosts file into string:[]string map
-func InitHostsJSON(path string) (map[string][]string, error) {
+func InitHostsJSON(path string) ([]*Vhost, error) {
 	var hosts map[string][]string
 
 	err := parseJSONFile(path, &hosts)
@@ -36,8 +46,7 @@ func InitHostsJSON(path string) (map[string][]string, error) {
 		return nil, err
 	}
 
-	return hosts, nil
-
+	return hostsMapToStructArray(hosts), nil
 }
 
 //ParseJSON
@@ -59,4 +68,43 @@ func parseJSONFile(path string, v interface{}) error {
 	}
 
 	return nil
+}
+
+func hostsMapToStructArray(hosts map[string][]string) []*Vhost {
+	vhostArray := make([]*Vhost, amountOfDomainsChecked(hosts))
+
+	i := 0
+	for hostname, domains := range hosts {
+
+		for j := range domains {
+			if len(domains[j]) == 0 {
+				continue
+			}
+
+			domain, port := splitPortAndDomain(domains[j])
+			vhostArray[i] = &Vhost{
+				Hostname: hostname,
+				Domain:   domain,
+				Port:     port,
+			}
+			i++
+		}
+	}
+	return vhostArray
+}
+
+func amountOfDomainsChecked(hosts map[string][]string) int {
+	cumulatedLength := 0
+	for _, v := range hosts {
+		cumulatedLength += len(v)
+	}
+	return cumulatedLength
+}
+
+func splitPortAndDomain(domain string) (string, string) {
+	split := strings.Split(domain, ":")
+	if len(split) == 1 {
+		return split[0], "443"
+	}
+	return split[0], split[1]
 }
